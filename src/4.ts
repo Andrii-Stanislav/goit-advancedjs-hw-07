20fd6af3e27fba359b22b11e32f5c4e04bae151f
Andrii Stanislav <139786374+Andrii-Stanislav-CodeAndCakes@users.noreply.github.com>
2024-01-30 23:03:08 +0100

    finish-hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[];
+
+  comeIn(person: Person) {
+    if (!this.door) return;
+
+    this.tenants.push(person);
+  }
+
+  openDoor(key: Key) {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+//
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +55,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
